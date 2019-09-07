337eb03b2e1d4ad83c107ee26a07ca5bd416356d
Sam Jackson <samuelsjackson7@gmail.com>
2019-09-07 15:41:44 -0400

    Set up server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENv === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every other requrest to the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
