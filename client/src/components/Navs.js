1c73e332ee87fcab7fd00fa01e82ffbd0a7d2a31
Sam Jackson <samuelsjackson7@gmail.com>
2019-09-16 21:20:38 -0400

    updated resume

--- client/src/components/Navs.js
+++ client/src/components/Navs.js
@@ -4,15 +4,17 @@ import { Navbar, Nav } from "react-bootstrap";
 export default function Navs() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand><h1>Samuel Jackson</h1></Navbar.Brand>
+      <Navbar.Brand>
+        <h1>Samuel Jackson</h1>
+      </Navbar.Brand>
       <Navbar id="responsive-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/">About</Nav.Link>
-          <Nav.Link href="/portfolio">
-            Portfolio
-          </Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
           <Nav.Link href="https://github.com/samj7792">GitHub</Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/samuel-s-jackson/">LinkedIn</Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/samuel-s-jackson/">
+            Linkedin
+          </Nav.Link>
         </Nav>
       </Navbar>
     </Navbar>
